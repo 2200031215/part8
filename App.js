289d5c5132c09e537af29381d2e991872ed9c809
2200031215 <147698689+2200031215@users.noreply.github.com>
2023-10-27 21:46:22 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div>
+      <h1>State Management with useState</h1>
+     <Counter/> 
+    </div>
+  );
+}
+
+export default App;
